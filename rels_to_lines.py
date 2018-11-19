bdf36bafb93fd5cfd8c6c03bf7f60a1f22930234
simberaj <simbera.jan@gmail.com>
2018-11-19 17:32:12 +0200

    relations to lines improvement to explicit OD ID column naming

--- rels_to_lines.py
+++ rels_to_lines.py
@@ -24,7 +24,7 @@ parser.add_argument('linefile',
     help='path to output the line CSV'
 )
 parser.add_argument('-n', '--name', default='site_id',
-    help='site id column in site file and the common suffix of the from_ and to_ columns in the relation file'
+    help='site id column in site file'
 )
 parser.add_argument('-x', '--xcol', default='x',
     help='name of the X-coordinate column in site file'
@@ -32,18 +32,28 @@ parser.add_argument('-x', '--xcol', default='x',
 parser.add_argument('-y', '--ycol', default='y',
     help='name of the Y-coordinate column in site file'
 )
+parser.add_argument('-f', '--from-id', 
+    help='name of the from column in relation file'
+)
+parser.add_argument('-t', '--to-id',
+    help='name of the to column in relation file'
+)
 
 if __name__ == '__main__':
     args = parser.parse_args()
     reldf = pd.read_csv(args.relfile, sep=';')
     sitedf = pd.read_csv(args.sitefile, sep=';')
+    from_id = args.from_id if args.from_id else 'from_' + args.name
+    to_id = args.to_id if args.to_id else 'to_' + args.name
     # print(sitedf.rename(lambda colname: str(colname) + '_from').dtypes)
     alldf = reldf.merge(
         sitedf.rename(columns=lambda colname: 'from_' + str(colname)),
-        on='from_' + args.name,
+        left_on=from_id,
+        right_on='from_' + args.name,
     ).merge(
         sitedf.rename(columns=lambda colname: 'to_' + str(colname)),
-        on='to_' + args.name,
+        left_on=to_id,
+        right_on='to_' + args.name,
     )
     alldf['line'] = [makeline(*vals) for vals in zip(
         alldf['from_' + args.xcol],
