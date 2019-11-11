cf3a9b66d3f969fbbeb469895ad06b0d2f766509
simberaj <simbera.jan@gmail.com>
2019-11-11 08:23:16 +0100

    remove olds

--- util/test_hier.py
+++ util/test_hier.py
@@ -6,38 +6,44 @@ import numpy
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import mobilib.hierarchy
 
-# rels = mobilib.region.Relations(numpy.array([
-    # [30, 3, 4, 2, 1, 0, 1, 1],
-    # [ 6,15,10, 3, 2, 1, 0, 1],
-    # [ 7, 9,12, 3, 1, 2, 0, 0],
-    # [10, 3, 2,10, 0, 1, 2, 1],
-    # [ 5, 8, 3, 1, 9, 4, 0, 0],
-    # [ 3, 3, 7, 1, 2, 8, 1, 0],
-    # [ 4, 0, 1, 6, 0, 1,10, 2],
-    # [ 3, 1, 1, 5, 0, 1, 1, 6],
-# ]))
 rels = mobilib.hierarchy.Relations(numpy.array([
-    [30, 3, 4, 2, 1, 0, 1, 1, 0],
-    [ 6,15,10, 3, 2, 1, 0, 1, 0],
-    [ 7, 9,12, 3, 1, 2, 0, 0, 0],
-    [10, 3, 2,10, 0, 1, 2, 1, 0],
-    [ 5, 8, 3, 1, 9, 4, 0, 0, 0],
-    [ 3, 3, 7, 1, 2, 8, 1, 0, 0],
-    [ 4, 0, 1, 6, 0, 1,10, 2, 0],
-    [ 3, 1, 1, 5, 0, 1, 1, 6, 0],
-    [ 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [30, 3, 4, 2, 1, 0, 1, 1],
+    [ 6,15,10, 3, 2, 1, 0, 1],
+    [ 7, 9,12, 3, 1, 2, 0, 0],
+    [10, 3, 2,10, 0, 1, 2, 1],
+    [ 5, 8, 3, 1, 9, 4, 0, 0],
+    [ 3, 3, 7, 1, 2, 8, 1, 0],
+    [ 4, 0, 1, 6, 0, 1,10, 2],
+    [ 3, 1, 1, 5, 0, 1, 1, 6],
 ]))
+print(rels.matrix.sum(axis=0))
+print(rels.matrix.sum(axis=1))
+rels = mobilib.hierarchy.Relations(numpy.array([
+    [30, 3, 4, 2, 1, 0, 1, 1, 0, 0],
+    [ 6,15,10, 3, 2, 1, 0, 1, 0, 0],
+    [ 7, 9,12, 3, 1, 2, 0, 0, 0, 0],
+    [10, 3, 2,10, 0, 1, 2, 1, 0, 0],
+    [ 5, 8, 3, 1, 9, 4, 0, 0, 0, 0],
+    [ 3, 3, 7, 1, 2, 8, 1, 0, 0, 0],
+    [ 4, 0, 1, 6, 0, 1,10, 2, 0, 0],
+    [ 3, 1, 1, 5, 0, 1, 1, 6, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 2, 4],
+]))
+# parents = numpy.array((0,0,1,0,1,2,3,3,8))
+# organ = numpy.array((0,0,1,0,0,0,0,0,0)).astype(bool)
 parents = numpy.array((0,0,1,0,1,2,3,3))
 organ = numpy.array((0,0,1,0,0,0,0,0)).astype(bool)
-# parents = numpy.array((0,0,1,1,1,1,1,1))
-# organ = numpy.array((0,0,1,1,0,0,0,0)).astype(bool)
-hierarchy = mobilib.hierarchy.Hierarchy.create(parents, organ)
-aggcrit = mobilib.hierarchy.WeightAggregationCriterion(150)
+hierarchy = mobilib.hierarchy.Hierarchy.create(parents, organ, weights=rels.matrix.sum(axis=0))
+
+evalcrit = mobilib.hierarchy.TransitionCriterion(organic_tolerance=1)
+print(evalcrit.evaluate_nodes(hierarchy, rels))
+
+aggcrit = mobilib.hierarchy.WeightAggregationCriterion(50)
 
 # hierarchy = mobilib.region.MaxflowHierarchyBuilder().build(rels)
 # hier2 = hierarchy.copy()
 # hierarchy = mobilib.region.MaxflowStochasticHierarchyBuilder().build(rels)
-# criterion = mobilib.region.TransitionCriterion(organic_tolerance=1)
 
 # print((numpy.where(numpy.isfinite(hierarchy.binding_matrix(rels)), hierarchy.binding_matrix(rels), 0) * 100).astype(int))
 print(hierarchy.structure_string())
