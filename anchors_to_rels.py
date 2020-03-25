76465b6331830f6b8702381234f960bc462942a1
simberaj <simbera.jan@gmail.com>
2020-03-25 23:32:09 +0100

    relation generation refactor/update, added singlepath approach

--- anchors_to_rels.py
+++ anchors_to_rels.py
@@ -1,12 +1,13 @@
 
 '''Generates location relation tables from an anchor point table.'''
 
+import os
 import argparse
 
 import numpy
 import pandas as pd
 
-import mobilib.relations as rels
+import mobilib.relations
 
 
 
@@ -34,47 +35,66 @@ parser.add_argument('-t', '--type',
     default='type', help='anchor point type column', metavar='COLNAME'
 )
 parser.add_argument('-H', '--home',
-    default=rels.DEFAULT_HOME_CODE,
+    default=mobilib.relations.DEFAULT_HOME_CODE,
     metavar='CODE',
     help='home anchor point type code'
 )
 parser.add_argument('-W', '--work',
-    default=rels.DEFAULT_WORK_CODE,
+    default=mobilib.relations.DEFAULT_WORK_CODE,
     metavar='CODE',
     help='work anchor point type code'
 )
 parser.add_argument('-M', '--multi',
-    default=rels.DEFAULT_MULTIFX_CODE,
+    default=mobilib.relations.DEFAULT_MULTIFX_CODE,
     metavar='CODE',
     help='multifunctional anchor point type code'
 )
+parser.add_argument('-x', '--exclude-selfinter', action='store_true',
+    help='do not generate self-interactions'
+)
+parser.add_argument('-g', '--generator',
+    help='single generator to use'
+)
+
+
+def generate_all(df, generators, site_id, user_id, importance, type, **kwargs):
+    sites = numpy.sort(df[site_id].unique())
+    n_sites = len(sites)
+    series = []
+    for gener in generators:
+        print(gener.name)
+        matrix = numpy.zeros((n_sites, n_sites))
+        for uid, subdf in df.groupby(user_id):
+            ids = subdf[site_id].values
+            rels = gener.relate(
+                subdf[importance].values,
+                subdf[type].values,
+                ids=ids,
+            )
+            site_indexes = numpy.searchsorted(sites, ids)
+            if rels is not None:
+                for src_i, tgt_i in enumerate(site_indexes):
+                    matrix[tgt_i,site_indexes] += rels[src_i]
+        rowis, colis = numpy.nonzero(matrix)
+        series.append(pd.Series(
+            matrix[rowis,colis],
+            index=[sites[rowis],sites[colis]],
+            name=gener.name,
+        ))
+    out_df = series[0]
+    for ser in series[1:]:
+        out_df = pd.merge(out_df, ser, left_index=True, right_index=True, how='outer')
+    out_df.index.names = ['from_' + site_id, 'to_' + site_id]
+    return out_df.fillna(0).reset_index()
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    home_codes = rels.build_codes(args.home, args.multi)
-    work_codes = rels.build_codes(args.work, args.multi)
-    geners = [
-        rels.GeneralRelationGenerator(),
-        rels.HomeBaseRelationGenerator(home_codes),
-        rels.HomeWorkRelationGenerator(home_codes, work_codes),
-    ]
+    generators = mobilib.relations.default_generators(
+        args.home, args.work, args.multi, not args.exclude_selfinter
+    )
+    if args.generator:
+        generators = [g for g in generators if g.name == args.generator]
     df = pd.read_csv(args.infile, sep=';')
-    sites = numpy.sort(df[args.site_id].unique())
-    n_sites = len(sites)
-    for gener in geners:
-        matrix = numpy.zeros((n_sites, n_sites))
-        for uid, subdf in df.groupby(args.user_id):
-            site_indexes = numpy.searchsorted(sites, subdf[args.site_id].values)
-            rels = gener.relate(
-                subdf[args.importance].values,
-                subdf[args.type].values
-            )
-            for i in site_indexes:
-                matrix[i,site_indexes] += rels[i]
-        rowis, colis = numpy.nonzero(matrix)
-        outfname = args.outfile + '_' + gener.name + '.csv'
-        pd.DataFrame({
-            'from_' + args.site_id : sites[rowis],
-            'to_' + args.site_id : sites[colis],
-            args.importance : matrix[rowis,colis],
-        }).to_csv(outfname, index=False, sep=';')
+    rels_df = generate_all(df, generators, **vars(args))
+    rels_df.to_csv(args.outfile, index=False, sep=';')
--- mobilib/relations.py
+++ mobilib/relations.py
@@ -1,118 +1,225 @@
-import numpy
+from typing import Dict, Optional
+
+import numpy as np
+import pandas as pd
 
 DEFAULT_HOME_CODE = 'k'
 DEFAULT_WORK_CODE = 't'
 DEFAULT_MULTIFX_CODE = 'm'
 
 
-class UntypedAimer:
-    def aim(self, types):
-        return numpy.ones(types.shape)
-    
+class Aimer:
+    def aim(self, types: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+
+
+class UntypedAimer(Aimer):
+    def aim(self, types: np.ndarray) -> np.ndarray:
+        return np.ones(types.shape)
+
     def __repr__(self):
         return '<UntypedAimer>'
 
-        
-class TypedAimer:
-    def __init__(self, type_fractions):
+
+class TypedAimer(Aimer):
+    def __init__(self, type_fractions: Dict[str, float]):
         self.type_fractions = type_fractions
-        
-    def aim(self, types):
-        return numpy.stack([
+
+    def aim(self, types: np.ndarray) -> np.ndarray:
+        return np.stack([
             (types == code) * weight
             for code, weight in self.type_fractions.items()
         ]).sum(axis=0)
-        
+
     def __repr__(self):
         return '<Aimer{}>'.format(self.type_fractions)
 
-        
-GENERAL_AIMER = UntypedAimer()
-        
-EE_HOME_AIMER = TypedAimer({
-    'k' : 1,
-    'm' : .5,
-})
 
-EE_WORK_AIMER = TypedAimer({
-    't' : 1,
-    'm' : .5,
-})
-
-def ipf(values, rowsums, colsums, tol=1e-9):
-    while True:
-        prevalues = values
+def ipf(values: np.ndarray,
+        rowsums: np.ndarray,
+        colsums: np.ndarray,
+        tol: float = 1e-9,
+        max_iter: int = 100,
+        ) -> np.ndarray:
+    # print((values * 1000).astype(int))
+    # print(rowsums, colsums)
+    rowsums = rowsums.reshape(-1, 1)
+    colsums = colsums.reshape(1, -1)
+    prevalues = values.copy()
+    for i in range(max_iter):
+        prevalues[:] = values[:]
         prerowsums = values.sum(axis=1)
-        values = values * (
-            rowsums / numpy.where(prerowsums == 0, 1, prerowsums)
-        ).reshape(-1, 1)
+        values *= rowsums / np.where(prerowsums == 0, 1, prerowsums).reshape(-1, 1)
+        # print('R')
+        # print((values * 1000).astype(int))
         precolsums = values.sum(axis=0)
-        values = values * (
-            colsums / numpy.where(precolsums == 0, 1, precolsums)
-        ).reshape(1, -1)
+        values *= colsums / np.where(precolsums == 0, 1, precolsums).reshape(1, -1)
+        # print('C')
+        # print((values * 1000).astype(int))
         diff = abs(prevalues - values).sum()
         if diff <= tol:
             prerowsums = values.sum(axis=1)
-            values = values * (
-                rowsums / numpy.where(prerowsums == 0, 1, prerowsums)
-            ).reshape(-1, 1)
-            print(values)
-        
+            values *= rowsums / np.where(prerowsums == 0, 1, prerowsums).reshape(-1, 1)
+            # print(i)
+            break
+    # print((values * 1000).astype(int))
+    # print((values.sum(axis=1) * 1000).astype(int))
+    # print((values.sum(axis=0) * 1000).astype(int))
+    return values
 
-class RelationGenerator:
-    def __init__(self, name=None, source_aimer=GENERAL_AIMER, target_aimer=GENERAL_AIMER, selfinter=True):
+
+class AimedRelationGenerator:
+    def __init__(self,
+                 name: Optional[str] = None,
+                 source_aimer: Aimer = UntypedAimer(),
+                 target_aimer: Aimer = UntypedAimer(),
+                 selfinter: bool = True,
+                 ):
         self.name = name
         self.source_aimer = source_aimer
         self.target_aimer = target_aimer
         self.selfinter = selfinter
-        
-    def relate(self, importances, types):
+
+    def eligibility(self,
+                    n: int,
+                    ids: Optional[np.ndarray] = None
+                    ) -> np.ndarray:
+        elig = np.ones((n,n), dtype=bool)
+        if not self.selfinter:
+            if ids is None:
+                np.fill_diagonal(elig, False)
+            else:
+                elig &= (ids.reshape(-1,1) != ids.reshape(1,-1))
+        return elig
+
+    def __repr__(self) -> str:
+        return  '<{0.__class__.__name__}({0.name},{0.source_aimer}-{0.target_aimer},selfinter={0.selfinter})>'.format(self)
+
+
+class ProportionalRelationGenerator(AimedRelationGenerator):
+    def relate(self,
+               importances: np.ndarray,
+               types: np.ndarray,
+               ids: Optional[np.ndarray] = None,
+               ) -> np.ndarray:
         n = len(importances)
-        rels = self.eligibility(n).astype(float)
-        sources = importances * self.source_aimer.aim(types) * rels.max(axis=1)
+        elig = self.eligibility(n, ids).astype(float)
+        sources = importances * self.source_aimer.aim(types) * elig.max(axis=1)
         source_sum = sources.sum()
         if source_sum == 0:
-            return numpy.zeros((n,n))
-        targets = importances * self.target_aimer.aim(types) * rels.max(axis=0)
+            return None
+        targets = importances * self.target_aimer.aim(types) * elig.max(axis=0)
         target_sum = targets.sum()
         if target_sum == 0:
-            return numpy.zeros((n,n))
-        result = ipf(rels, sources / source_sum, targets / target_sum)
+            if self.selfinter:
+                return np.diag(sources / source_sum)
+            else:
+                return None
+        result = ipf(elig, sources / source_sum, targets / target_sum)
         return result
-        
-    def eligibility(self, n):
-        elig = numpy.ones((n,n), dtype=bool)
-        if not self.selfinter:
-            numpy.fill_diagonal(elig, False)
-        return elig
-        
-    def __repr__(self):
-        return '<RelGen({0.name},{0.source_aimer}-{0.target_aimer},selfinter={0.selfinter})>'.format(self)
 
 
+class MaximumRelationGenerator(AimedRelationGenerator):
+    def relate(self,
+               importances: np.ndarray,
+               types: np.ndarray,
+               ids: Optional[np.ndarray] = None,
+               ) -> Optional[np.ndarray]:
+        n = len(importances)
+        elig = self.eligibility(n, ids).astype(float)
+        sources = importances * self.source_aimer.aim(types) * elig.max(axis=1)
+        source_sum = sources.sum()
+        if source_sum == 0:
+            return None
+        has_unique_ids = (ids is None or len(np.unique(ids)) == n)
+        # get largest eligible source
+        if has_unique_ids:
+            src_i = sources.argmax()
+        else:
+            src_i = np.flatnonzero(
+                ids == pd.Series(sources).groupby(ids).sum().idxmax()
+            )[0]
+        targets = importances * self.target_aimer.aim(types) * elig.max(axis=0)
+        target_sum = targets.sum()
+        if target_sum <= targets[src_i]:   # only the source is a valid target
+            if self.selfinter:
+                tgt_i = src_i
+            else:
+                return None
+        else:
+            targets[src_i] = 0
+            # get largest (still) eligible target
+            if has_unique_ids:
+                tgt_i = targets.argmax()
+            else:
+                tgt_i = np.flatnonzero(
+                    ids == pd.Series(targets).groupby(ids).sum().idxmax()
+                )[0]
+        rels = np.zeros((n,n))
+        rels[src_i,tgt_i] = 1
+        return rels
 
+
+def default_generators(home_code: str = DEFAULT_HOME_CODE,
+                       work_code: str = DEFAULT_WORK_CODE,
+                       multi_code: str = DEFAULT_MULTIFX_CODE,
+                       multi_home_frac: float = .5,
+                       selfinter: bool = True
+                       ):
+    home_aimer = TypedAimer({
+        home_code: 1,
+        multi_code: multi_home_frac,
+    })
+    work_aimer = TypedAimer({
+        work_code: 1,
+        multi_code: (1 - multi_home_frac),
+    })
+    return [
+        ProportionalRelationGenerator('general',
+            selfinter=selfinter,
+        ),
+        ProportionalRelationGenerator('homebased',
+            source_aimer=home_aimer,
+            selfinter=selfinter,
+        ),
+        ProportionalRelationGenerator('homework',
+            source_aimer=home_aimer,
+            target_aimer=work_aimer,
+            selfinter=selfinter,
+        ),
+        MaximumRelationGenerator('genfirst',
+            source_aimer=home_aimer,
+            selfinter=selfinter,
+        ),
+        MaximumRelationGenerator('homefirst',
+            source_aimer=home_aimer,
+            selfinter=selfinter,
+        ),
+        MaximumRelationGenerator('homeworkfirst',
+            source_aimer=home_aimer,
+            target_aimer=work_aimer,
+            selfinter=selfinter,
+        ),
+    ]
 
 
 if __name__ == '__main__':
-    imps = numpy.array([4,2,2,1,1,1])
-    codes = numpy.array([
+    imps = np.array([4,2,2,1,1,1])
+    codes = np.array([
         DEFAULT_HOME_CODE,
         DEFAULT_WORK_CODE,
         DEFAULT_MULTIFX_CODE,
         DEFAULT_WORK_CODE,
-        '',
+        DEFAULT_HOME_CODE,
         '',
     ])
-    gens = [
-        RelationGenerator(),
-        RelationGenerator(selfinter=False),
-        RelationGenerator('home', EE_HOME_AIMER),
-        RelationGenerator('home', EE_HOME_AIMER, selfinter=False),
-        RelationGenerator('hw', EE_HOME_AIMER, EE_WORK_AIMER),
-        RelationGenerator('hw', EE_HOME_AIMER, EE_WORK_AIMER, selfinter=False),
-    ]
-    for gen in gens:
-        print(gen)
-        print((gen.relate(imps, codes) * 1000).astype(int))
+    ids = np.array([1,2,3,4,4,5])
+    for selfinter in (True, False):
+        for gen in default_generators(selfinter=selfinter):
+            print(gen)
+            rels = gen.relate(imps, codes, ids)
+            if rels is not None:
+                print((rels * 1000).astype(int))
+            else:
+                print(rels)
     print()
-            
